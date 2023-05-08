2b34734ba7d9e7e779892ee845ff5eb1bb292c80
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-08 21:27:24 +0530

    restart with timer is not working

--- shutdown.py
+++ shutdown.py
@@ -9,7 +9,11 @@ root.config(bg='#ffba08')
 
 def restart():
     os.system('shutdown /r /t 1')
-    
+
+
+def restart_in():
+    time = timer.get()
+    os.system('shutdown /r /t time')
 
 
 shutdown = Button(root, text="ShutDown", bg="#9d0208", fg='white', font=(
@@ -24,9 +28,9 @@ restart = Button(root, text="Restart", bg="#9d0208", fg='white', font=(
     'arial', 20, 'bold'), border=7, relief='raised', command=restart)
 restart.place(x=150, y=300, width=200, height=50)
 
-restart_timer = Button(root, text="Restart  in ", bg="#9d0208", fg='white', font=(
-    'arial', 20, 'bold'), border=7, relief='raised')
-restart_timer.place(x=150, y=400, width=200, height=50)
+restart_in = Button(root, text="Restart  in ", bg="#9d0208", fg='white', font=(
+    'arial', 20, 'bold'), border=7, relief='raised', command=restart_in)
+restart_in.place(x=150, y=400, width=200, height=50)
 
 timer = Entry(root, bg="#ffd000", fg='black', font=(
     'arial', 20, 'bold'), border=7, relief='raised')
